d28b8beef0eaef5bdb7fc5fdddcb56342d4a36e1
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2024-11-01 02:59:54 +0000

    testing changes 2

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
+function upstairs() {
+    /*userChoice = prompt("")
+    if (userChoice == "1") {
+        upstairs()
+    } else  if (userChoice == "2") {
+        basement()
+    }*/
+   alert("upstairs")
+}
+
+function basement() {
+    /*userChoice = prompt("")
+    if (userChoice == "1") {
+        upstairs()
+    } else  if (userChoice == "2") {
+        basement()
+    }*/
+   alert("basement")
+}
+
+
 function startGame() {
     let name = prompt("What is your name?")
     alert("Welcome to the Haunted House Choose-Your-Own-Adventure Game! Follow the instructions exactly to ensure your safe escape!")
 
     let userChoice = prompt("You walk through the door and BAM it slams shut, locking you inside. You must now find another escape, do you (1) go upstairs or (2) go into the basement?")
     if (userChoice == "1") {
-        console.log("upstairs")
+        upstairs()
     } else  if (userChoice == "2") {
-        console.log("basement")
+        basement()
     }
 }
 
