bc171bb2a6d6bf070cf3eaa30ec3ce569d3736a9
haya raham asif <haya.raham-asif@stu.ocsb.ca>
2024-11-01 02:54:26 +0000

    testing changes 1

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+function startGame() {
+    let name = prompt("What is your name?")
+    alert("Welcome to the Haunted House Choose-Your-Own-Adventure Game! Follow the instructions exactly to ensure your safe escape!")
 
+    let userChoice = prompt("You walk through the door and BAM it slams shut, locking you inside. You must now find another escape, do you (1) go upstairs or (2) go into the basement?")
+    if (userChoice == "1") {
+        console.log("upstairs")
+    } else  if (userChoice == "2") {
+        console.log("basement")
+    }
+}
+
+startGame()
